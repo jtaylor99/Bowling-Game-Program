06bc049df3b0cad31be3c19758fe6f20306489b3
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-08-01 11:18:36 -0500

    I had to change the shell

--- shell.py
+++ shell.py
@@ -3,14 +3,17 @@ import core
 
 def main():
     print('Welcome to the Bowling Alley!')
-    print('One of the best Local Bowling Alley')
-    players = input('How players do you have ')
-    while True:
-        if players.isdigit():
-            print('Ok, go to for your shoes')
-            break
-        else:
-            print('Please choose a valid option!')
+    print('Let\'s begin')
+    frames = []
+    for frame in range(1, 11):
+        print('frame', frame)
+        frame = []
+        for _ in range(2):
+            throw = int(input('Pins:'))
+            frame.append(throw)
+        frames.append(frame)
+
+    print(frames)
 
 
 if __name__ == '__main__':
