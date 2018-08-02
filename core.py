1fa43ea707309ac291fa7a94f243fa17ec683f2a
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-08-02 16:21:23 -0500

    I changed the core

--- core.py
+++ core.py
@@ -1,7 +1,31 @@
-from random import randint
+def bowlers_turn():
+    total = 0
+    for frame in range(1, 11):
+        print('frame', frame)
+        frame = []
+        for _ in range(2):
+            throw = int(input('Pins:'))
+
+            frame.append(total)
+        if throw > 10:
+            print('Oops! you can\'t go over 10')
 
 
-def lane():
-    ball_roll = randint(range(10))
-    if ball_roll < 10:
-        return 'points'
+def bowlers_score():
+    total = 0
+    frames = []
+    for i in range(len(frames)):
+        if '/' in frames[i]:
+            ball_1 = frames[i][0]
+            ball_2 = 10 - ball_1
+            total += ball_1 + ball_2
+            if i + 1 < len(frames):
+                next_ball = frames[i + 1][0]
+                total += next_ball
+            elif 'x' in frames[i]:
+                ball_1 = 10
+                total += ball_1
+
+            else:
+                total += sum(frames[i])
+    print(total)
